7423a84245ec406fe9682c852db04a40a016a44a
SACHIN <madhan123455@gmail.com>
2021-11-28 18:02:53 +0530

    Improved App.js and App.css code

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,54 @@
 import './App.css';
 import { useState} from "react";
+import validator from 'validator';
 import Axios from 'axios';
 
 function App() {
-  const [empId, setEmpId] = useState(0);
+  const [empId, setEmpId] = useState('');
   const [firstName, setfirstName] = useState("");
   const [surName, setsurName] = useState("");
   const [email, setEmail] = useState("");
-  const [dob, setDob] = useState(0);
+  const [dob, setDob] = useState('');
   const [gender, setGender] = useState("");
+  const [buttonState , setButtonState] = useState('Create');
+  const [status , setStatus] = useState('Create new Employee');
+  const [hideEmpId , setHideEmpId] = useState(true);
 
+  const readUpdateDeleteEmp = (mode) => {
+    clearFields();
+    switch(mode){
+      case 'Read':
+        setStatus( 'Read Existing Employee');
+        setButtonState("Read");
+        break;
+       case 'Update' :
+         setStatus("Update Existing Employee");
+         setButtonState("Update");
+         break;
+       case 'Delete':
+         setStatus( "Delete Existing Employee");
+         setButtonState("Delete");
+         break;  
+       default:
+         setStatus('Create New Employee');
+         setButtonState("Create");
+         setHideEmpId(true);
+    } 
+  }
+
+const clearFields = () =>{
+  setEmpId('');
+  setfirstName("");
+  setsurName("");
+  setEmail("");
+  setDob('');
+  setGender("");
+  setHideEmpId(false);
+}
   
 const createEmp =() =>{
-  console.log(empId);
 Axios.post('http://localhost:3000/employees', {
-  empId : empId,
+ empId : empId,
  firstName: firstName,
  surName:surName, 
  email:email,
@@ -22,48 +56,79 @@ Axios.post('http://localhost:3000/employees', {
  gender: gender
 })
 .then(()=>{
-  console.log("success");
+  alert(`Employee ${firstName+' '+surName} created successfully`);
 });
 };
 
 const readEmp =() =>{
   Axios.get('http://localhost:3000/employees/'+empId).then((res)=>{
   console.log(res);
+  setfirstName(res.data.firstName);
+  setsurName(res.data.surName);
+  setEmail(res.data.email);
+  setDob(res.data.dob);
+  setGender(res.data.gender);
 },
 (error) => {
 console.log(error);
 })
 }
-const updateEmp = () =>{
-   Axios.put(('http://localhost:3000/employees/'+empId),{
-    firstName,surName,email,dob,gender }).then((res)=>{
-  console.log(res);
-},
-(error) => {
-console.log(error);
-})
+const updateDelEmployee = () =>{
+  switch(buttonState){
+    case 'Update' :
+      Axios.put(('http://localhost:3000/employees/'+empId),{
+           firstName,surName,email,dob,gender }).then((res)=>{
+             console.log(res);
+             },
+            (error) => {
+            console.log(error);
+           })
+          break;
+  case 'Delete' :
+      Axios.delete('http://localhost:3000/employees/'+empId).then((res)=>{
+        console.log(res);
+         },
+         (error) => {
+             console.log(error);
+          })
+         break;
+  default:
+      createEmp();
+}}
 
-}
-const deleteEmp =() =>{
-  Axios.delete('http://localhost:3000/employees/'+empId).then((res)=>{
-  console.log(res);
-},
-(error) => {
-console.log(error);
-})
-}  
+const wrapperFunction = ( ) => {
+  if ( buttonState !== 'Delete') {
+       if(validator.isEmail(email))
+          {
+             updateDelEmployee();
+           } 
+        else {
+             alert("Incorrect email");
+           }}
+else{
+  updateDelEmployee();
+}};
+
+
+const showButtonUp = hideEmpId ?{display :'none'} :{display:'block' ,float:'right'};
+const showButtonDown = buttonState==='Read' ? {display:'none' }: {display:'block' ,float:'right'};
   return (
     <div className="App">
       <div className="App-header">
         
         <h1>Employee Management </h1> <hr />
-        <button onClick={createEmp} >Create</button>
-        <button onClick={readEmp} >Read</button>
-        <button onClick={updateEmp} >Update</button>
-        <button onClick={deleteEmp} >Delete</button> <hr />
-       <br /> <hr />
+        <button onClick={readUpdateDeleteEmp.bind(this,'Create')} >Create</button>
+        <button onClick={readUpdateDeleteEmp.bind(this,'Read')} >Read</button>
+        <button onClick={readUpdateDeleteEmp.bind(this,'Update')} >Update</button>
+        <button onClick={readUpdateDeleteEmp.bind(this,'Delete')} >Delete</button> <hr />
+       <br /> 
+       <div className='Status-Updater'>
+       <p>{status}</p>   
+       <hr />
+       </div>
        </div>
        <div className='information'>
+              
        <table>
         <tbody>
       <tr>
@@ -71,7 +136,7 @@ console.log(error);
         <label> Employee ID  : </label>
         </td>
       <td>
-        <input type="text" onChange={(event)=>{
+        <input type="text" value={empId} onChange={(event)=>{
           setEmpId(event.target.value);
         }}/>
         </td>
@@ -80,12 +145,18 @@ console.log(error);
       </tbody>
       <tbody>
       <tr>
+             <td></td>
+             <td style={showButtonUp}><input type='button' value='Read'  onClick={readEmp} /></td>
+           </tr>  
+        </tbody>
+      <tbody>
+      <tr>
       
       <td> 
         <label> First Name   : </label>
         </td>
       <td>
-        <input type="text" onChange={(event)=>{
+        <input type="text" value={firstName} onChange={(event)=>{
           setfirstName(event.target.value);
         }}/>
         </td>
@@ -98,7 +169,7 @@ console.log(error);
         <label> Last Name  :</label>
         </td>
       <td>
-        <input type="text" onChange={(event)=>{
+        <input type="text" value={surName} onChange={(event)=>{
           setsurName(event.target.value);
         }}/>
         </td>
@@ -109,7 +180,7 @@ console.log(error);
         <label> E-Mail  :</label>
         </td>
       <td>
-        <input type="text" onChange={(event)=>{
+        <input type="text" value={email} onChange={(event)=>{
           setEmail(event.target.value);
         }}/>
         </td>
@@ -120,7 +191,7 @@ console.log(error);
         <label> DOB  :</label>
         </td>
       <td>
-        <input type="date" onChange={(event)=>{
+        <input type="date" value={dob} onChange={(event)=>{
           setDob(event.target.value);
         }}/>
         </td>
@@ -132,18 +203,26 @@ console.log(error);
         <label> Gender  :</label>
         </td>
       <td>
-        <input style={{fontSize:20}} type="radio" onChange={(event)=>{
+        <input style={{fontSize:20}} type="radio" value='Male' onChange={(event)=>{
           setGender(event.target.value);
         }}/>
         <label> Male </label>
-        <input type="radio" value="Female" onChange={(event)=>{
-          setGender(event.target.value);
+        <input type="radio" value="Female"  onChange={(event)=>{
+          setGender(event.target.value); 
         }}/>
         <label> Female</label>
         </td>
       </tr>
       </tbody>
-            </table>
+      <tbody>
+        <tr>
+          <td></td>
+          <td>
+          <input type='button'  style={showButtonDown} value={buttonState  } onClick={ wrapperFunction } />
+          </td>
+        </tr>
+      </tbody>
+     </table>
       </div>
     </div>
   );
